d9cc49b3f2adbb390c19d26713a56b5f0de5a4b8
Guadalupe Rocha <guadaluperocha@GuadalupesMBP2.attlocal.net>
2022-07-02 13:22:41 -0500

    list-issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -2,14 +2,30 @@ var repoNameEl = document.querySelector("#repo-name");
 var limitWarningEl = document.querySelector("#limit-warning");
 var issueContainerEl = document.querySelector("#issues-container");
 
+getRepoName = function() { 
+    // grab repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+
+    if(repoName) {
+        // display repo name on the page
+        repoNameEl.textContent = repoName;
+
+        getRepoIssues(repoName);
+    } else {
+        // if no repo was given, redirect to the homepage
+        document.location.replace("./index.html");
+    }
+};
+
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
 
+    // make a get request to url 
     fetch(apiUrl).then(function(response) {
         // request was successful
         if (response.ok) {
           response.json().then(function(data) {
-            // pass response data to dom function
             displayIssues(data);
 
             // check if api has paginated issues
@@ -18,8 +34,8 @@ var getRepoIssues = function(repo) {
             }
           });
         } else {
-            console.log(response);
-            alert("There was a problem with your request!");
+            // if not successful, redirect to homepage
+            document.location.replace("./index.html");
         }
     });
 };
@@ -75,4 +91,5 @@ var displayWarning = function(repo) {
     limitWarningEl.appendChild(linkEl);
 };
 
-getRepoIssues("facebook/react");
+getRepoName();
+
